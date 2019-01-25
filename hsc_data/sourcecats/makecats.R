83e5ebe7442df5622fe5de53755d910c0ba6c2ad
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-01-24 16:47:35 -0800

    source extractor check image outputs

--- hsc_data/sourcecats/makecats.R
+++ hsc_data/sourcecats/makecats.R
@@ -4,11 +4,16 @@
 indir = normalizePath("../calexp")
 fnames = grep(".fits.fz", dir("../calexp"), value=TRUE)
 sex = "/usr/bin/sextractor" # local SEx binary
+funpack = "/usr/bin/funpack" # local FITS unpack binary
 
 # loop
 for(i in 1:length(fnames)){
     
-    comm = paste0(sex, " -c sex.config -PARAMETERS_NAME sex.param ", indir, "/", fnames[i])
+    ftemp = strsplit(fnames[i], ".fz")[[1]]
+    fcat = paste0(strsplit(fnames[i], ".fits.fz")[[1]], ".dat")
+    system(paste0(funpack, " -O ", getwd(), "/", ftemp, " ", indir, "/", fnames[i]))
+    system(paste0(sex, " -c default.sex -CATALOG_NAME ", fcat, " -CATALOG_TYPE ASCII -CHECKIMAGE_TYPE SEGMENTATION -CHECKIMAGE_NAME test.fits ", ftemp))
+    unlink(ftemp)
     
 }
 
