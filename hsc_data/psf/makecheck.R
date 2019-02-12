956ef40f4652f6c168188360c94535989bb663c4
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-02-11 23:34:12 -0800

    reduction of PSF check from 101x101 to 55x55 pixel

--- hsc_data/psf/makecheck.R
+++ hsc_data/psf/makecheck.R
@@ -6,12 +6,12 @@ require("astro", quietly=TRUE)
 # definitions
 funpack = "/usr/bin/funpack"
 inbase = "calexp-HSC-R-8283-38"
-sampfz = paste0(inbase, ".pssample.fits.fz")
-residfz = paste0(inbase, ".psresid.fits.fz")
+sampfz = paste0(inbase, ".pssample-55.fits.fz")
+residfz = paste0(inbase, ".psresid-55.fits.fz")
 samp = paste0(inbase, ".pssample.fits")
 resid = paste0(inbase, ".psresid.fits")
 psf = paste0(inbase, ".psf.fits")
-size = 101 # vignette size in samp/resid
+size = 55 # vignette size in samp/resid
 
 # setup
 system(paste0(funpack, " -O ", samp, " ", sampfz))
@@ -32,7 +32,7 @@ xdim = ydim = 55
 xcen = ycen = (xdim+1)/2
 counts = rbind((psfdat[xcen,ycen:1]), (psfdat[xcen,ycen:ydim]), (psfdat[xcen:1,ycen]), (psfdat[xcen:xdim,ycen]))
 counts = apply(counts, 2, median)
-rads = 1:101 * 0.168
+rads = 1:size * 0.168
 sbs = sbslo = sbshi = rep(50, length(rads))
 sbs[1:length(counts)] = suppressWarnings(-2.5 * log10(counts / (0.168^2)) + 27)
 sbslo[1:length(counts)] = suppressWarnings(-2.5 * log10((counts+errs[,"ERR95LO"]) / (0.168^2)) + 27)
@@ -54,18 +54,18 @@ zlo = 1*-0.07
 zhi = 15*0.07
 cextext = 2
 insettext = c(0.5,0.5)
-insetscale = 7
+insetscale = 3
 scalelen = 5
 
-aimage(sampdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, xlo=1, ylo=1, xdim=9*101, ydim=3*101, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab=""); abline(h=seq(1,1000,by=101), col=linecol, lwd=linewd); abline(v=seq(1,1000,by=101), col=linecol, lwd=linewd); box(col="white", lwd=3)
+aimage(sampdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, xlo=1, ylo=1, xdim=9*size, ydim=3*size, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab=""); abline(h=seq(1,1000,by=size), col=linecol, lwd=linewd); abline(v=seq(1,1000,by=size), col=linecol, lwd=linewd); box(col="white", lwd=3)
 label("topleft", lab="Point Source Samples", col="white", cex=cextext, inset=insettext)
 
-aimage(psfdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab="", xdim=101, ydim=101, padvalue=0); box(col="white", lwd=3)
+aimage(psfdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab="", xdim=size, ydim=size, padvalue=0); box(col="white", lwd=3)
 label("topleft", lab="PSF", col="white", cex=cextext, inset=insettext)
 lines(x=c((par("usr")[2]-insetscale),((par("usr")[2]-insetscale-(scalelen/0.168)))), y=rep(par("usr")[3]+insetscale,2), lwd=5, lend=3, col="white")
-text(x=(par("usr")[2]-insetscale), y=(par("usr")[3]+insetscale+3), lab=paste(scalelen, "arcsec"), col="white", adj=c(1,0), cex=1.25)
+text(x=(par("usr")[2]-insetscale), y=(par("usr")[3]+insetscale+2), lab=paste(scalelen, "arcsec"), col="white", adj=c(1,0), cex=1.25)
 
-aimage(residdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, xlo=1, ylo=1, xdim=9*101, ydim=3*101, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab=""); abline(h=seq(1,1000,by=101), col=linecol, lwd=linewd); abline(v=seq(1,1000,by=101), col=linecol, lwd=linewd); box(col="white", lwd=3)
+aimage(residdat, col.map="sls", scale.type="asinh", scale.lo=zlo, scale.hi=zhi, xlo=1, ylo=1, xdim=9*size, ydim=3*size, smooth.fwhm=fwhm, axes=FALSE, xlab="", ylab=""); abline(h=seq(1,1000,by=size), col=linecol, lwd=linewd); abline(v=seq(1,1000,by=size), col=linecol, lwd=linewd); box(col="white", lwd=3)
 label("topleft", lab="Residuals", col="white", cex=cextext, inset=insettext)
 
 par("mar"=c(4.1,4.1,1,0.5))
