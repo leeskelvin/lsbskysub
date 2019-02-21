507b9d4dcee24886d80028ce0e101e7f5748301e
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-02-20 16:58:10 -0800

    numcounts update

--- hsc_data/numcounts/numcounts.R
+++ hsc_data/numcounts/numcounts.R
@@ -20,7 +20,7 @@ par("oma"=c(2.5,3.5,1,3.5))
 for(i in 1:length(infiles)){
     
     # plot
-    aplot(NA, xlim=c(15,29), ylim=c(10^0,10^6), log="y", yformat="p", las=1, type="n", xlab=bquote(paste("apparent magnitude : ", m[r])), ylab=bquote(paste(N[obj], " ", deg^{-2}, " ", mag^{-1})), xnmin=1, axes=FALSE)
+    aplot(NA, xlim=c(14.5,30.5), ylim=c(10^0,10^7), log="y", yformat="p", las=1, type="n", xlab=bquote(paste("apparent magnitude : ", m[r])), ylab=bquote(paste(N[obj], " ", deg^{-2}, " ", mag^{-1})), xnmin=1, axes=FALSE)
     mtext(side=3, line=0.25, text=c("low density region : 8283-38", "high-density region : 9592-20")[i])
     
     # raw data
@@ -32,26 +32,37 @@ for(i in 1:length(infiles)){
     # binned data
     breaks = c(seq(0,50,by=bw) - (bw/2), 50 + (bw/2))
     h = hist(mags, breaks=breaks, plot=FALSE)
+    binmag = h$mids
+    bindat = (h$counts / area) / bw
+    bindatlog = log10(bindat); if(any(bindatlog==-Inf)){bindatlog[bindatlog==-Inf] = 0}
     
     # power law line fit
+    magbright = 22
+    magfaint = 25
     fn = function(x, a){return(a + 0.4*x)}
-    
-    # low density missing
-    maglo = 22
-    maghi = 25
-    good = which(h$mids >= maglo & h$mids <= maghi)
-    data = log10(h$counts/area); if(any(data < 0)){data[data<0] = NA}
-    fdat = fit(data=data[good], par=list(a=1), fn=fn, arg=list(x=h$mids[good]))
+    good = which(binmag >= magbright & binmag <= magfaint)
+    fitdat = fit(data=bindatlog[good], par=list(a=1), fn=fn, arg=list(x=binmag[good]))
+    binexplog = fn(x=binmag, a=fitdat$par$a)
+    binexp = 10^binexplog
+    binfaint = binexp - bindat
+    binfaintlog = suppressWarnings(log10(binfaint))
     
     # plot
-    lines(h$mids-bw/2, h$counts/area, col=c("#5e3c99","#e66101")[i], type="s", lwd=2, lend=3)
-    lines(h$mids+bw/2, h$counts/area, col=c("#5e3c99","#e66101")[i], type="S", lwd=2, lend=3)
-    abline(a=fdat$par$a, b=0.4, col=c("#b2abd2","#fdb863")[i], lwd=1.5)
-    abline(v=c(maglo,maghi), lty=2, lend=3, lwd=1.5)
+    magupper = 30
+    lines(binmag[binmag>=magfaint & binmag<=magupper], binfaint[binmag>=magfaint & binmag<=magupper], col="grey90", type="h", lwd=7.5, lend=3)
+    lines(binmag-bw/2, bindat, col=c("#5e3c99","#e66101")[i], type="s", lwd=2, lend=3)
+    lines(binmag+bw/2, bindat, col=c("#5e3c99","#e66101")[i], type="S", lwd=2, lend=3)
+    abline(a=fitdat$par$a, b=0.4, col=c("#b2abd2","#fdb863")[i], lwd=1.5)
+    abline(v=c(magbright,magfaint), lty=2, lend=3, lwd=1.5)
+    
+    # legend
+    cex = 0.5
+    inset = c(0.025,0.025)
+    legend("topleft", legend=c("detected","","faint missing"), lty=c(1,1,NA), lwd=c(2,1.5,NA), col=c("#5e3c99","#b2abd2",NA), fill=c(NA,NA,"grey90"), border=NA, bty="n", inset=inset, cex=cex, merge=T)
+    legend("topleft", legend=bquote(paste("log"[10], N["obj"]," = ", .(formatC(fitdat$par$a,format="f",digits=2)), " + 0.4", m[r], sep="")), lwd=1.5, col=c("#b2abd2","#fdb863")[i], bty="n", inset=inset+c(0,0.031), cex=cex)
     
     # finish up
-    legend("bottomleft", legend=bquote(paste("log"[10], N["obj"]," = ", .(formatC(fdat$par$a,format="f",digits=2)), " + 0.4", m[r], sep="")), lwd=1.5, col=c("#b2abd2","#fdb863")[i], bty="n", inset=0.05, cex=0.5)
-    aaxes(las=1, yformat="p", labels=list(c(1,2),c(1,4))[[i]])
+    aaxes(las=1, yformat="p", labels=list(c(1,2),c(1,4))[[i]], xnmin=4)
     mtext(side=c(2,4)[i], line=2.5, text=bquote(paste(N[obj], " ", deg^{-2}, " ", mag^{-1})))
     
 }
