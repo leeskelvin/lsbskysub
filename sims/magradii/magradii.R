2cb34e189de55ecf8e42f3d504ae5e637ca89b87
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-03-04 00:09:29 -0800

    radii plots

--- sims/magradii/magradii.R
+++ sims/magradii/magradii.R
@@ -2,6 +2,8 @@
 
 # setup
 require("astro", quietly=TRUE)
+#palette(c("#000000","#f1a340","#998ec3","#edf8b1","#7fcdbb","#2c7fb8"))
+set.seed(3125)
 
 # definitions
 datas = paste0("../../hsc_data/calexp/", grep(".dat", dir("../../hsc_data/calexp/"), value=TRUE))
@@ -16,30 +18,35 @@ dat1 = read.table(datas[1], stringsAsFactors=FALSE)
 dat2 = read.table(datas[2], stringsAsFactors=FALSE)
 dat = rbind(dat1, dat2)
 colnames(dat) = c("NUMBER", "FLUX_AUTO", "MAG_AUTO", "KRON_RADIUS", "PETRO_RADIUS", "BACKGROUND", "THRESHOLD", "X_IMAGE", "Y_IMAGE", "A_IMAGE", "B_IMAGE", "THETA_IMAGE", "ELLIPTICITY", "CLASS_STAR", "FLUX_RADIUS")
+if(any(dat[,"MAG_AUTO"] == 99)){dat = dat[-which(dat[,"MAG_AUTO"]==99),]}
+attach(dat)
+FLUX_RADIUS = FLUX_RADIUS * pixelsize # arcsec
+MAG_AUTO = MAG_AUTO + 27 # zero point correction
 
 # samples
-main = which(dat[,"MAG_AUTO"]+27 >= magbright+bw/2)
-toobright = which(dat[,"MAG_AUTO"]+27 <= magbright+bw/2)
+main = which(MAG_AUTO >= magbright+bw/2)
+toobright = which(MAG_AUTO <= magbright+bw/2)
 
 # fit
-fn = function(x, a, b){return(a + b*x)}
-#err = sqrt(dat[main,"FLUX_RADIUS"]+1)
-err = abs(dat[main,"MAG_AUTO"]+27-28)
-fitdat = fit(dat[main,"FLUX_RADIUS"]*pixelsize, par=list(a=1, b=1), fn=fn, arg=list(x=dat[main,"MAG_AUTO"]+27), sigma=err, method="BFGS")
-xx = seq(10,50,len=100)
-yy = fn(x=xx, a=fitdat$par$a, b=fitdat$par$b)
+fit = lm(FLUX_RADIUS[main] ~ MAG_AUTO[main])
 
 # dev
-cairo_pdf(file="magradii.pdf", width=5, height=5)
-par("mar"=c(4,4,1,4))
+cairo_pdf(file="magradii.pdf", width=5, height=4.5)
+par("mar"=c(3.5,3.5,1,3.5))
 
 # plot
-aplot(NA, xlim=c(16,28), ylim=c(0.11,3), log="y", xlab=bquote(paste("apparent magnitude : ", m[r])), ylab=bquote(paste("half light radius : ", r[e], " / arcsec")), las=1, xnmin=1, axes=FALSE)
-points(dat[main,"MAG_AUTO"]+27, dat[main,"FLUX_RADIUS"]*pixelsize, pch=".", cex=2, col=acol("black",0.5))
-points(dat[toobright,"MAG_AUTO"]+27, dat[toobright,"FLUX_RADIUS"]*pixelsize, pch=".", cex=2, col=acol("red",0.5))
+palette(c("#000000","#f1a340","#998ec3","#edf8b1","#7fcdbb","#2c7fb8"))
+aplot(NA, xlim=c(16,28), ylim=c(0.11,3), log="y", xlab=bquote(paste("apparent magnitude : ", m[r])), ylab=bquote(paste("half light radius : ", r[e], " / arcsec")), las=1, xnmin=1, axes=FALSE, mgp=c(1.75,0.25,0))
+points(MAG_AUTO[main], FLUX_RADIUS[main], pch=".", cex=2, col=col2rgba(1,0.5))
+points(MAG_AUTO[toobright], FLUX_RADIUS[toobright], pch=".", cex=2, col=col2rgba(2,0.5))
+
+
+
 lines(xx,yy,col="blue", lwd=3)
-aaxes(side=1:3, xnmin=1)
-aaxis(side=4, 
+aaxes(side=1:3, xnmin=1, las=1)
+aaxis(side=4, fn=function(x){x/pixelsize}, las=1)
+abox()
+mtext(side=4, line=1.75, text=bquote(paste(r[e], " / pixel")))
 
 # finish up
 graphics.off()
