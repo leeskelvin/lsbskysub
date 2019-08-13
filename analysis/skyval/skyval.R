8b84d4c1d2fbd2dab9b62aa2e42c47bb85b65dd9
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-08-13 13:38:21 -0400

    skyval update

--- analysis/skyval/skyval.R
+++ analysis/skyval/skyval.R
@@ -12,7 +12,8 @@ dat = readRDS("../datacollect/skydata.rds")
 fn = function(flux){-2.5*log10(flux/(0.168^2))+27}
 ordera = c(1,5,3,7)
 orderb = c(2,6,4,8)
-ydats = list(dat$skymean[,ordera],dat$skymean[,orderb])
+yvals = list(dat$skymean[,ordera],dat$skymean[,orderb])
+ystds = list(dat$skystd[,ordera],dat$skystd[,orderb])
 pdfnames = paste0("skyval-", c("a","b","a5","b5"), ".pdf")
 ylimlo = c(29.6,32.5,29.6,32.5)
 ylimhi = c(27.6,28.5,27.6,28.5)
@@ -25,7 +26,7 @@ metcol = c(2,2,2,2,3,3,3,3,1,1)
 simsources = paste0("Simulating ", c("All Sources","Bright Only (No EBL)","All","Bright Only (No EBL)"))
 
 # loop
-for(i in 1:length(ydats)){
+for(i in 1:length(yvals)){
     
     # dev
     cairo_pdf(file=pdfnames[i], width=8, height=6)
@@ -33,37 +34,47 @@ for(i in 1:length(ydats)){
     # par
     layout(rbind(c(1,2),c(3,4)))
     palette(c("#000000", "#e66101", "#5e3c99", "#fdb863", "#b2abd2", "#edf8b1", "#7fcdbb", "#2c7fb8"))
-    par("mar"=c(0.25,0.25,0.25,0.25))
-    par("oma"=c(1,4.5,7.5,4.5))
+    par("mar"=c(0.75,0.25,0.75,0.25))
+    par("oma"=c(5.5,4.5,3,4.5))
     
     # loop
-    for(j in 1:ncol(ydats[[i]])){
+    for(j in 1:ncol(yvals[[i]])){
         
         # base plot setup
         if(j==1){labs = c(2)}
         if(j==2){labs = c(4)}
         if(j==3){labs = c(2)}
         if(j==4){labs = c(4)}
-        aplot(NA, xlim=c(1-0.25,nrow(ydats[[1]])+0.25), ylim=c(ylimlo[i],ylimhi[i]), xlab="", ylab="", las=1, side=NA, log="", labels=labs, tick=FALSE, bty="n")
+        aplot(NA, xlim=c(1-0.25,nrow(yvals[[1]])+0.25), ylim=c(ylimlo[i],ylimhi[i]), xlab="", ylab="", las=1, side=NA, log="", labels=labs, tick=FALSE, bty="n")
         abline(h=seq(27.5,35,by=0.5), col="grey75", lty=2, lend=1)
         #abline(v=c(0.00001,0.0001,0.001,0.01,0.1,1), col="grey75", lty=2, lend=1)
         aaxes(side=c(2,4), labels=labs, ynmin=1, las=1, mgp=c(2,0.5,0))
         if(j%in%c(1,2)){mtext(side=3, line=0.25, text=c("low density","high density")[j], cex=1)}
         if(j%in%c(1,2)){labtext="exponential"}else{labtext="de Vaucouleurs"}
-        label("topleft", lab=labtext, inset=0.5, outline="white")
+        label("topright", lab=labtext, inset=c(2,0.5), outline="white")
         abox()
         
         # points
-        ydat = suppressWarnings(fn(ydats[[i]][,j]))
-        toolow = is.na(ydat) | ydat>35
-        if(any(toolow)){
-            ydat[toolow] = par("usr")[3]-0.5
+        ydat = suppressWarnings(fn(yvals[[i]][,j]))
+        ydatlo = suppressWarnings(fn(yvals[[i]][,j] - (ystds[[i]][,j]) ))
+        ydathi = suppressWarnings(fn(yvals[[i]][,j] + (ystds[[i]][,j]) ))
+        if( any(is.na(ydat) | ydat>35) ){ ydat[is.na(ydat) | ydat>35] = par("usr")[3]+0.5 }
+        if( any(is.na(ydatlo) | ydatlo>35) ){ ydatlo[is.na(ydatlo) | ydatlo>35] = par("usr")[3]+0.5 }
+        if( any(is.na(ydathi) | ydathi>35) ){ ydathi[is.na(ydathi) | ydathi>35] = par("usr")[3]+0.5 }
+        for(l in 1:length(ydatlo)){
+            suppressWarnings(arrows(x0=l, y0=ydatlo[l], y1=ydathi[l], code=3, length=0.05, angle=90, lwd=lwd*0.75, lend=1, col=col2hex(metcol[l],alpha=1), lty="21"))
         }
-        apoints(1:nrow(ydats[[i]]), ydat, pch=metpch, lwd=lwd, cex=cex, lend=1, col=metcol)
-        if(length(toolow) > 0){
-            for(k in which(toolow)){
-                arrows(x0=(1:nrow(ydats[[i]]))[k], y0=ydat[k], y1=par("usr")[3], lwd=lwd, cex=cex, lend=1, col=metcol[k], length=0.075)
-            }
+        apoints(1:nrow(yvals[[i]]), ydat, pch=metpch, lwd=lwd, cex=cex, lend=1, col=metcol)
+#        if(length(toolow) > 0){
+#            for(k in which(toolow)){
+#                arrows(x0=(1:nrow(yvals[[i]]))[k], y0=ydat[k], y1=par("usr")[3], lwd=lwd, cex=cex, lend=1, col=metcol[k], length=0.075)
+#            }
+#        }
+        
+        if(j <= 2){
+            par("xpd"=NA)
+            apoints(1:nrow(yvals[[i]]), rep((par("usr")[3]+0.275),nrow(yvals[[i]])), pch=metpch, lwd=lwd, cex=cex, lend=1, col=metcol)
+            par("xpd"=FALSE)
         }
         
     }
@@ -73,7 +84,7 @@ for(i in 1:length(ydats)){
     #mtext(side=1, line=2, text="sky estimation method", outer=T)
     mtext(side=3, line=1, text=simsources[i], outer=T)
     mtext(side=2, line=2.5, text=bquote(paste("mean subtracted sky level / mag ", arcsec^{-2})), outer=T)
-    alegend("top", legend=names(dat$files), ncol=4, byrow=T, inset=0.5, outer=TRUE, seg.len=0.8, seg.gap=0.4,  type=setNames(apply(cbind(pch=metpch,lwd=lwd,col=metcol,cex=cex), 1, as.list), rep("p",length(dat$files))), bty="o", cex=0.9, box.pad=0.25, line.spacing=1.25)
+    alegend("bottom", legend=names(dat$files), ncol=4, byrow=T, inset=0.5, outer=TRUE, seg.len=0.8, seg.gap=0.4,  type=setNames(apply(cbind(pch=metpch,lwd=lwd,col=metcol,cex=cex), 1, as.list), rep("p",length(dat$files))), bty="o", cex=0.9, box.pad=0.25, line.spacing=1.25)
     
     # finish up
     graphics.off()
